057757aec14e076dfae610ce208cae900a767c00
syuan0619 <max911114@gmail.com>
2023-04-27 23:27:46 +0800

    剩註冊還沒好
    4/27

---
+++ 碳排放/registercheck.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+$ID = $_POST['ID'];
+$Password = $_POST['Password'];
+$Name = $_POST['Name'];
+$Email = $_POST['Email'];
+$Phone = $_POST['Phone'];
+
+if ($account != null && $password != null) {
+    $link = mysqli_connect("localhost", "root", "", "sa");
+    $sql  = "insert into user (ID, Password, Name, Phone,Email) values ('$ID', '$Password', '$Name', '$Phone','$Email')";
+?>
+    <?php
+    if (mysqli_query($link, $sql)) {
+    ?>
+        <script>
+            alert("註冊成功!");
+            location.href = "login.php";
+        </script>
+    <?php
+    } else {
+    ?>
+        <script>
+            alert("註冊失敗!");
+            location.href = "login.php";
+        </script>
+    <?php
+
+    }
+    ?>
+<?php
+}
+?>
